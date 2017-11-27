b501e8ff1348820c2aa85021226e38dea9181090
Guilherme Melo <ghmelo77@gmail.com>
2017-11-27 03:16:40 -0200

    Criação do projeto springboot para lembrete de livros

---
+++ src/main/java/com/lembreteslivros/LembreteslivrosApplication.java
@@ -0,0 +1,12 @@
+package com.lembreteslivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembreteslivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembreteslivrosApplication.class, args);
+	}
+}
